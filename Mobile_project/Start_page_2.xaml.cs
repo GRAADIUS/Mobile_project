4b78c7cfe573c40313071a88e359589178c0224b
opilane <opilane@HP-CZC2349HTG>
2024-02-08 15:33:39 +0200

    save

---
+++ Mobile_project/Start_page_2.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_project
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Start_page_2 : ContentPage
+    {
+        List<ContentPage> Pages = new List<ContentPage>()
+        {
+            new Entry_page(), new Time_page(), new ViewBox_page(), new date_time_page(), new Stepper_slider_page()
+        };
+        List<string> texts = new List<string>()
+        {
+            "Entry leht", "Time leht", "ViewBox leht", "Clicker leht", "Date and time page", "Stepper and slider page"
+        };
+        StackLayout st;
+        public Start_page_2()
+        {
+            st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Pink
+            };
+            for (int i = 0; i < Pages.Count; i++)
+            {
+                Button btn = new Button
+                {
+                    Text = texts[i],
+                    BackgroundColor = Color.LightPink,
+                    TextColor = Color.Black,
+                    TabIndex = i
+                };
+                st.Children.Add(btn);
+                btn.Clicked += Ava_vajav_leht;
+            }
+            ScrollView sv = new ScrollView();
+            Content = sv;
+        }
+        private async void Ava_vajav_leht(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            await Navigation.PushAsync(Pages[button.TabIndex]);
+        }
+    }
+}
---
+++ Mobile_project/date_time_page.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_project
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class date_time_page : ContentPage
+    {
+        DatePicker dp;
+        TimePicker tp;
+        Label lbl;
+        public date_time_page()
+        {
+            dp = new DatePicker
+            {
+                Format="D",
+                MinimumDate = DateTime.Now.AddDays(-10),
+                MaximumDate = DateTime.Now.AddDays(10),
+                TextColor= Color.Beige,
+            };
+            dp.DateSelected += Dp_DateSelected; ;
+            tp = new TimePicker
+            {
+                Time = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second)
+            };
+            tp.PropertyChanged += Tp_PropertyChanged;
+            lbl = new Label
+            {
+                BackgroundColor= Color.Pink,
+            };
+            AbsoluteLayout abs = new AbsoluteLayout
+            {
+                Children= { lbl, dp, tp }
+            };
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.1, 0.2, 200, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(dp, new Rectangle(0.1, 0.5, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(dp, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(tp, new Rectangle(0.5, 0.7, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(tp, AbsoluteLayoutFlags.PositionProportional);
+            Content = abs;
+        }
+
+        private void Dp_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            lbl.Text = e.NewDate.ToString("D");
+        }
+
+        private void Tp_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            lbl.Text = "Aeg: " + tp.Time.ToString();
+        }
+    }
+}
